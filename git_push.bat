35f6466a52641ba132402e3d504a1cd0bb600315
Darkzveller <elkattoufi.youssef@gmail.com>
2026-03-19 21:50:33 +0100

    partie c simu, entierement faite

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "partie c simu, calcule de Vtn, kn et tracé de srt(id) vs Vgs" && git push
+git status && git add . && git commit -m "partie c simu, entierement faite" && git push
