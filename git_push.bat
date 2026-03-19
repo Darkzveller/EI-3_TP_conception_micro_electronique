c3010c8712ed9baba5369b8e6db6253dfe4acb42
Darkzveller <elkattoufi.youssef@gmail.com>
2026-03-19 21:00:04 +0100

    partie c simu, calcule de Vtn, kn et trac├® de srt(id) vs Vgs

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "sauvegarde au cas ou" && git push
+git status && git add . && git commit -m "partie c simu, calcule de Vtn, kn et tracé de srt(id) vs Vgs" && git push
