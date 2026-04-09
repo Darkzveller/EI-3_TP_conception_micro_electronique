eedd85e038bc3f80dbdb020b3351a773a423c4b5
Darkzveller <elkattoufi.youssef@gmail.com>
2026-04-09 20:55:29 +0200

    Partie A faite est fini

---
+++ Sujet TP et fichiers_Sylleyman/Codes dacquisition/instrument_setting.py
@@ -0,0 +1,17 @@
+#####################
+# Current compliance#
+#####################
+CURRENT_COMPLIANCE=0.5
+
+##########################
+# Gate Voltage Control#
+##########################
+GATEVOLTAGE_MIN=2
+GATEVOLTAGE_MAX=2
+GATEVOLTAGE_STEP=1
+##########################
+# Drain Voltage Control#
+##########################
+DRAINVOLTAGE_MIN= 0
+DRAINVOLTAGE_MAX= 3.5
+DRAINVOLTAGE_STEP= 0.25
