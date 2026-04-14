3673385279543ec5ff34ba26a1f426939227f3f6
Darkzveller <elkattoufi.youssef@gmail.com>
2026-04-14 22:56:38 +0200

    Ajout code acquisition + modif et ajout commentaire C3

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "Partie D1 faite + C1 pensez a mettre en forme les commentaires + ajout fichier mamadou" && git push
+git status && git add . && git commit -m "Ajout code acquisition + modif et ajout commentaire C3" && git push
