6eee756153358bfd2414e009a9afe7dac79e49bc
Darkzveller <elkattoufi.youssef@gmail.com>
2026-04-17 11:58:56 +0200

    Modif nom dosier 'Bjt_fait_par_moi' opur Bjt + ajout mesures fait au compte rendu

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "Ajout code d'acquistion + dossier wassim + dossier old" && git push
+git status && git add . && git commit -m "Modif nom dosier 'Bjt_fait_par_moi' opur Bjt + ajout mesures fait au compte rendu" && git push
