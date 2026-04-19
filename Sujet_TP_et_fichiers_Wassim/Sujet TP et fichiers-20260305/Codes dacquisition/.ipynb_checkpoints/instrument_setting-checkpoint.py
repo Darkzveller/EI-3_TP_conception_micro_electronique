5f392cc600a1dca5a57478cef4f8726ba3c1b9fb
Darkzveller <elkattoufi.youssef@gmail.com>
2026-04-19 21:23:33 +0200

    Ajout calcul th├®orique E1 + dezippae fichier wassim

---
+++ Sujet_TP_et_fichiers_Wassim/Sujet TP et fichiers-20260305/Codes dacquisition/.ipynb_checkpoints/instrument_setting-checkpoint.py
@@ -0,0 +1,40 @@
+#####################
+# Current compliance#
+#####################
+#CURRENT_COMPLIANCE = 0.5
+
+##########################
+# Gate Voltage Control#
+##########################
+#GATEVOLTAGE_MIN  = 0
+#GATEVOLTAGE_MAX  = 5
+#GATEVOLTAGE_STEP = 0.1
+
+##########################
+# Drain Voltage Control#  
+##########################
+#DRAINVOLTAGE_MIN  = 5
+#DRAINVOLTAGE_MAX  = 5
+#DRAINVOLTAGE_STEP = 0.1
+################################################# IDVG
+
+
+#####################
+# Current compliance#
+#####################
+CURRENT_COMPLIANCE = 0.5
+
+##########################
+# Gate Voltage Control#
+##########################
+GATEVOLTAGE_MIN  = 2.5
+GATEVOLTAGE_MAX  = 3.5
+GATEVOLTAGE_STEP = 0.5
+
+##########################
+# Drain Voltage Control#  
+##########################
+DRAINVOLTAGE_MIN  = 0
+DRAINVOLTAGE_MAX  = 5
+DRAINVOLTAGE_STEP = 0.1
+############################################### IDVD
--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "Ajout code python modifier" && git push
+git status && git add . && git commit -m "Ajout calcul théorique E1 + dezippae fichier wassim" && git push
